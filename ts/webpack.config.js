85a12edf9e89d4e2c8f3fcf82259b1bb86cd249b
Tom Houlé <tom@tomhoule.com>
2017-10-17 22:43:12 +0200

    Set up babel

--- ts/webpack.config.js
+++ ts/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './ts/src/main.tsx',
-        // 'edition-new': './js/src/edition-new.ts',
+    // 'edition-new': './js/src/edition-new.ts',
     // },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -10,18 +10,44 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.ts', '.tsx'],
-        modules: ['./node_modules', './ts/src'],
+            modules: ['./node_modules', './ts/src'],
     },
     devtool: 'source-map',
     module: {
         rules: [
-            { test: /.tsx?/, use: 'awesome-typescript-loader' },
+            {
+                test: /.js/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['env', { targets: { browsers: ['IE 9'] } }],
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /.tsx?/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['env', { targets: { browsers: ['IE 9'] } }],
+                            ],
+                        },
+                    },
+                    { loader: 'awesome-typescript-loader' },
+                ],
+            },
         ],
     },
-     devServer: {
-         // contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
-         // compress: true, // enable gzip compression
-         historyApiFallback: true, // true for index.html upon 404, object for multiple paths
-         // hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-     },
+    devServer: {
+        // contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
+        // compress: true, // enable gzip compression
+        historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+        // hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    },
 }
