63ce9e23c586e7f6ce663afd5cb9d75b1952012c
Tom Houlé <tom@tomhoule.com>
2017-10-20 00:26:15 +0200

    wip - rpc with rest proxy

--- Justfile
+++ Justfile
@@ -1,37 +1,58 @@
 clean-proto:
-    rm -rf ts/src/rpc
-    mkdir -p ts/src/rpc
+  rm -rf ts/src/rpc
+  mkdir -p ts/src/rpc
 
 compile-proto: clean-proto
-    protoc \
-        --rust_out=src/rpc \
-        --grpc_out=src/rpc \
-        --plugin=protoc-gen-grpc=`which grpc_rust_plugin` \
-        -I ./proto \
-        ./proto/*.proto
-    protoc \
-        --plugin=protoc-gen-ts=`which protoc-gen-ts` \
-        --js_out=import_style=commonjs,binary:ts/src/rpc \
-        --ts_out=service=true:ts/src/rpc \
-        -I ./proto \
-        ./proto/*.proto
+  # Proxy grpc stub
+  protoc -I/usr/local/include -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -Iproto \
+    --go_out=plugins=grpc:. \
+    ./proto/repository.proto
+
+  # Proxy
+  protoc -I/usr/local/include -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -Iproto \
+    --grpc-gateway_out=logtostderr=true:. \
+    ./proto/repository.proto
+
+  # Swagger
+  protoc -I/usr/local/include -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -Iproto \
+    --swagger_out=logtostderr=true:. \
+    ./proto/repository.proto
+
+  # Rust
+  protoc \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -Iproto \
+    --rust_out=src/rpc \
+    --grpc_out=src/rpc \
+    --plugin=protoc-gen-grpc=`which grpc_rust_plugin` \
+    ./proto/repository.proto
 
 start-postgres:
-    sudo systemctl start postgresql
+  sudo systemctl start postgresql
 
 start-proxy:
-    grpcwebproxy \
-        --server_tls_cert_file=./misc/localhost.crt \
-        --server_tls_key_file=./misc/localhost.key \
-        --server_http_debug_port=8081 \
-        --backend_addr=localhost:9090 \
-        --backend_tls_noverify
+  grpcwebproxy \
+    --server_tls_cert_file=./misc/localhost.crt \
+    --server_tls_key_file=./misc/localhost.key \
+    --server_http_debug_port=8081 \
+    --backend_addr=localhost:9090 \
+    --backend_tls_noverify
 
 watch:
-    watchexec -c --exts rs --restart "cargo run --bin repository"
+  watchexec -c --exts rs --restart "cargo run --bin repository"
 
 watch-web:
-    watchexec -c --exts rs,hbs --restart "cargo run --bin locutions-server"
+  watchexec -c --exts rs,hbs --restart "cargo run --bin locutions-server"
 
 write-schema:
-    diesel print-schema > src/db/schema.rs
+  diesel print-schema > src/db/schema.rs
---
+++ src/rpc/repository_grpc.rs
@@ -0,0 +1,98 @@
+// This file is generated. Do not edit
+// @generated
+
+// https://github.com/Manishearth/rust-clippy/issues/702
+#![allow(unknown_lints)]
+#![allow(clippy)]
+
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
+#![allow(box_pointers)]
+#![allow(dead_code)]
+#![allow(missing_docs)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(trivial_casts)]
+#![allow(unsafe_code)]
+#![allow(unused_imports)]
+#![allow(unused_results)]
+
+const METHOD_ETHICS_REPOSITORY_GET_SCHEMA: ::grpcio::Method<super::repository::GetSchemaParams, super::repository::EthicsSchema> = ::grpcio::Method {
+    ty: ::grpcio::MethodType::Unary,
+    name: "/repository.EthicsRepository/GetSchema",
+    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+};
+
+const METHOD_ETHICS_REPOSITORY_GET_EDITIONS: ::grpcio::Method<super::repository::GetEditionsParams, super::repository::Editions> = ::grpcio::Method {
+    ty: ::grpcio::MethodType::Unary,
+    name: "/repository.EthicsRepository/GetEditions",
+    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+};
+
+pub struct EthicsRepositoryClient {
+    client: ::grpcio::Client,
+}
+
+impl EthicsRepositoryClient {
+    pub fn new(channel: ::grpcio::Channel) -> Self {
+        EthicsRepositoryClient {
+            client: ::grpcio::Client::new(channel),
+        }
+    }
+
+    pub fn get_schema_opt(&self, req: super::repository::GetSchemaParams, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::repository::EthicsSchema> {
+        self.client.unary_call(&METHOD_ETHICS_REPOSITORY_GET_SCHEMA, req, opt)
+    }
+
+    pub fn get_schema(&self, req: super::repository::GetSchemaParams) -> ::grpcio::Result<super::repository::EthicsSchema> {
+        self.get_schema_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn get_schema_async_opt(&self, req: super::repository::GetSchemaParams, opt: ::grpcio::CallOption) -> ::grpcio::ClientUnaryReceiver<super::repository::EthicsSchema> {
+        self.client.unary_call_async(&METHOD_ETHICS_REPOSITORY_GET_SCHEMA, req, opt)
+    }
+
+    pub fn get_schema_async(&self, req: super::repository::GetSchemaParams) -> ::grpcio::ClientUnaryReceiver<super::repository::EthicsSchema> {
+        self.get_schema_async_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn get_editions_opt(&self, req: super::repository::GetEditionsParams, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::repository::Editions> {
+        self.client.unary_call(&METHOD_ETHICS_REPOSITORY_GET_EDITIONS, req, opt)
+    }
+
+    pub fn get_editions(&self, req: super::repository::GetEditionsParams) -> ::grpcio::Result<super::repository::Editions> {
+        self.get_editions_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn get_editions_async_opt(&self, req: super::repository::GetEditionsParams, opt: ::grpcio::CallOption) -> ::grpcio::ClientUnaryReceiver<super::repository::Editions> {
+        self.client.unary_call_async(&METHOD_ETHICS_REPOSITORY_GET_EDITIONS, req, opt)
+    }
+
+    pub fn get_editions_async(&self, req: super::repository::GetEditionsParams) -> ::grpcio::ClientUnaryReceiver<super::repository::Editions> {
+        self.get_editions_async_opt(req, ::grpcio::CallOption::default())
+    }
+    pub fn spawn<F>(&self, f: F) where F: ::futures::Future<Item = (), Error = ()> + Send + 'static {
+        self.client.spawn(f)
+    }
+}
+
+pub trait EthicsRepository {
+    fn get_schema(&self, ctx: ::grpcio::RpcContext, req: super::repository::GetSchemaParams, sink: ::grpcio::UnarySink<super::repository::EthicsSchema>);
+    fn get_editions(&self, ctx: ::grpcio::RpcContext, req: super::repository::GetEditionsParams, sink: ::grpcio::UnarySink<super::repository::Editions>);
+}
+
+pub fn create_ethics_repository<S: EthicsRepository + Send + Clone + 'static>(s: S) -> ::grpcio::Service {
+    let mut builder = ::grpcio::ServiceBuilder::new();
+    let instance = s.clone();
+    builder = builder.add_unary_handler(&METHOD_ETHICS_REPOSITORY_GET_SCHEMA, move |ctx, req, resp| {
+        instance.get_schema(ctx, req, resp)
+    });
+    let instance = s.clone();
+    builder = builder.add_unary_handler(&METHOD_ETHICS_REPOSITORY_GET_EDITIONS, move |ctx, req, resp| {
+        instance.get_editions(ctx, req, resp)
+    });
+    builder.build()
+}
