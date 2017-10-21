4a0fde2e58b62369ab69f4c426316c496b7ad6ae
Tom Houlé <tom@tomhoule.com>
2017-10-22 01:22:08 +0200

    Cascade delete edition -> fragments

--- migrations/20170820170431_create_fragments/up.sql
+++ migrations/20170820170431_create_fragments/up.sql
@@ -2,7 +2,7 @@ ALTER TABLE editions ADD COLUMN slug varchar(200) NOT NULL UNIQUE;
 CREATE UNIQUE INDEX editions_slug_idx ON editions(slug);
 
 CREATE TABLE fragments (
-    edition_slug varchar(200) REFERENCES editions(slug) NOT NULL,
+    edition_slug varchar(200) REFERENCES editions(slug) ON DELETE CASCADE NOT NULL,
     fragment_path TEXT NOT NULL,
     value TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
