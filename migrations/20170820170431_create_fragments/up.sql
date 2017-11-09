9ca5c2c57549db7381e37cd13ae70bcc2490c21a
Tom Houlé <tom@tomhoule.com>
2017-11-09 23:17:00 +0100

    Fix fragments table primary key

--- migrations/20170820170431_create_fragments/up.sql
+++ migrations/20170820170431_create_fragments/up.sql
@@ -2,7 +2,7 @@ ALTER TABLE editions ADD COLUMN slug varchar(200) NOT NULL UNIQUE;
 CREATE UNIQUE INDEX editions_slug_idx ON editions(slug);
 
 CREATE TABLE fragments (
-  id UUID PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   edition_id UUID REFERENCES editions(id) ON DELETE CASCADE NOT NULL,
   fragment_path TEXT NOT NULL,
   value TEXT NOT NULL,
