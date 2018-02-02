6256111d66c7fa5ebe8cde08ff64d1a661434d32
Tom Houlé <tom@tomhoule.com>
2018-02-02 22:07:53 +0100

    Delete ts folder

--- ts/src/ethics/Edition.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-type Props = {}
-
-export const Edition = (props: Props) => <div>This is an edition</div>
-
-export default Edition
--- ts/src/types.ts
+++
@@ -1,9 +0,0 @@
-import * as epicUtils from 'epic-utils'
-import * as api from 'api-types'
-
-export interface InjectedDependencies {
-  get: typeof epicUtils.get
-  post: typeof epicUtils.post
-  put: typeof epicUtils.put
-  simpleGet: typeof epicUtils.simpleGet
-}
